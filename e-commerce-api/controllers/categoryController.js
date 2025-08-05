1cb03fdbb2ba24063f387088c9a2d2bfe7b15c78
Vignesh Bharathi <vigneshbharathi68@hotmail.com>
2025-08-05 21:45:07 +0530

    formatted code

--- e-commerce-api/controllers/categoryController.js
+++ e-commerce-api/controllers/categoryController.js
@@ -1,27 +1,27 @@
-const Category = require('../models/Category.js')
-
+const Category = require("../models/Category.js");
 
 exports.getAllCategories = async (req, res) => {
-    try {
-        const categories = await Category.find()
-        res.status(200).json(categories)
-
-    } catch (err) {
-        res.status(500).json({message: err.message})
-    }
-}
+  try {
+    const categories = await Category.find();
+    res.status(200).json(categories);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
 
 exports.addCategory = async (req, res) => {
-    try {
-        const slug = req.body.name.toLowerCase().replace(/ /g, '-')
-        const category = await Category.create({
-            name: req.body.name,
-            slug,
-            description: req.body.description,
-        })
-        res.status(200).json(category)
-    } catch (err) {
-        console.log({err})
-        res.status(504).json({ message: err?.message ?? err?.errorResponse?.errmsg });
-    }
-}
+  try {
+    const slug = req.body.name.toLowerCase().replace(/ /g, "-");
+    const category = await Category.create({
+      name: req.body.name,
+      slug,
+      description: req.body.description,
+    });
+    res.status(200).json(category);
+  } catch (err) {
+    console.log({ err });
+    res
+      .status(504)
+      .json({ message: err?.message ?? err?.errorResponse?.errmsg });
+  }
+};
